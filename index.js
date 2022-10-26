bab14ba1ae9c9f4e082b2384b45a563bf965a7d8
Adinortey Lawerteh <adinortey360@gmail.com>
2022-10-26 01:45:27 +0000

    add contact route

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
   res.send('Home')
 })
 
+//receive the data from contact form and send it to the email
+app.post('/contact', (req, res) => {
+    res.send('Contact')
+})
+
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
