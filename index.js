11d6c85cf05b29580f8532fab698a6d3fbe36620
Adinortey Lawerteh <adinortey360@gmail.com>
2022-10-26 02:09:21 +0000

    add nodemailer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var nodemailer = require('nodemailer');
 const app = express()
 const port = 3000
 
@@ -8,7 +9,28 @@ app.get('/', (req, res) => {
 
 //receive the data from contact form and send it to the email
 app.post('/contact', (req, res) => {
-    res.send('Contact')
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+        user: 'your email',
+        pass: 'your password'
+        }
+    });
+    
+    var mailOptions = {
+        from: 'your email',
+        to: 'receiver email',
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!'
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+        console.log(error);
+        } else {
+        console.log('Email sent: ' + info.response);
+        }
+    });
 })
 
 
